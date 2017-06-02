f3dc7ed9e6f9c7d500087bb1fb00bfbe25894dbb
Nickolas Komarnitsky <komarnickolas@gmail.com>
2017-06-02 17:28:39 -0600

    functional

--- js/main.js
+++ js/main.js
@@ -3,74 +3,138 @@
  */
 var hex = '#hhmmss';
 var time = 'hh:mm:ss';
+var hour = 12;
+var minute = 0;
+var second = 0;
+var hour_deg = hour * 30;
+var min_deg = minute * 6;
+var second_deg = second * 6;
+var selected = null;
+function $(el) {
+	return document.getElementById(el);
+}
+$('hour').addEventListener("click", function () {
+	$('hour_edit').classList.remove('hidden');
+	$('hour_edit').setAttribute("placeholder", this.innerHTML);
+	this.classList.add('hidden');
+},false);
+
+$('minute').addEventListener("click", function () {
+	$('minute_edit').classList.remove('hidden');
+	$('minute_edit').setAttribute("placeholder", this.innerHTML);
+	this.classList.add('hidden');
+},false);
+
+
+$('second').addEventListener("click", function () {
+	$('second_edit').classList.remove('hidden');
+	$('second_edit').setAttribute("placeholder", this.innerHTML);
+	this.classList.add('hidden');
+},false);
+
+$('hour_edit').addEventListener("keypress", function (e) {
+	if(e.keyCode === 13){
+		$('hour').classList.remove('hidden');
+		hour = this.value;
+		hour_deg = hour * 30;
+		$('hour-hand').style.transform = "rotate(" + hour_deg + 'deg)';
+		this.classList.add('hidden');
+	}
+},false);
+
+$('minute_edit').addEventListener("keypress", function (e) {
+	if(e.keyCode === 13){
+		$('minute').classList.remove('hidden');
+		minute = this.value;
+		min_deg = minute * 6;
+		$('minute-hand').style.transform = "rotate(" + min_deg + 'deg)';
+		this.classList.add('hidden');
+	}
+},false);
+
+$('second_edit').addEventListener("keypress", function (e) {
+	if(e.keyCode === 13){
+		$('second').classList.remove('hidden');
+		second = this.value;
+		second_deg = second * 6;
+		$('second-hand').style.transform = "rotate(" + second_deg + 'deg)';
+		this.classList.add('hidden');
+	}
+},false);
+
+$('second-hand').addEventListener("mousedown", function () {
+	selected = this;
+}, false);
+$('minute-hand').addEventListener("mousedown", function () {
+	selected = this;
+}, false);
+$('hour-hand').addEventListener("mousedown", function () {
+	selected = this;
+}, false);
+document.addEventListener("mousemove", function () {
+	if (selected !== null) {
+		switch (selected) {
+			case $('second-hand'):
+				selected.style.transform = "rotate(" + second_deg + 'deg)';
+				second_deg++;
+				if (second_deg % 6 === 0) {
+					second++;
+					if (second >= 60) {
+						second = 0;
+					}
+				}
+				break;
+			case $('minute-hand'):
+				selected.style.transform = "rotate(" + min_deg + 'deg)';
+				min_deg++;
+				if (min_deg % 6 === 0) {
+					minute++;
+					if (minute >= 60) {
+						minute = 0;
+					}
+				}
+				break;
+			case $('hour-hand'):
+				selected.style.transform = "rotate(" + hour_deg + 'deg)';
+				hour_deg++;
+				if (hour_deg % 30 === 0) {
+					hour++;
+					if (hour > 12) {
+						hour = 1;
+					}
+				}
+				break;
+		}
+	}
+	updateClock();
+}, false);
+document.addEventListener("mouseup", function () {
+	selected = null;
+}, false);
 function updateClock() {
-    var date = new Date();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-    if(minute < 10){
-        minute = "0"+minute;
-    }
-    if(hour < 10){
-        hour = "0" + hour;
-    }
-    if(second <10){
-        second = "0"+second;
-    }
-    hex = '#'+hour+minute+second;
-    time = hour+':'+minute+':'+second;
-    second_deg = second *6;
-    document.getElementById('minute-hand').style = "background-color: "+hex;
-    document.getElementById('hour-hand').style = "background-color: "+hex;
-    document.getElementById('digital').innerHTML = time;
-    document.getElementById('second-hand').style.webkitTransform = 'rotate('+second_deg+'deg)';
-    document.getElementById('second-hand').style.mozTransform    = 'rotate('+second_deg+'deg)';
-    document.getElementById('second-hand').style.msTransform     = 'rotate('+second_deg+'deg)';
-    document.getElementById('second-hand').style.oTransform      = 'rotate('+second_deg+'deg)';
-    document.getElementById('second-hand').style.transform       = 'rotate('+second_deg+'deg)';
-    if(second == 59){
-        min_deg = minute*6;
-        document.getElementById('minute-hand').style.webkitTransform = 'rotate('+min_deg+'deg)';
-        document.getElementById('minute-hand').style.mozTransform    = 'rotate('+min_deg+'deg)';
-        document.getElementById('minute-hand').style.msTransform     = 'rotate('+min_deg+'deg)';
-        document.getElementById('minute-hand').style.oTransform      = 'rotate('+min_deg+'deg)';
-        document.getElementById('minute-hand').style.transform       = 'rotate('+min_deg+'deg)';
-    }
-    if(minute == 59){
-        hour_deg = hour*30;
-        document.getElementById('hour-hand').style.webkitTransform = 'rotate('+hour_deg+'deg)';
-        document.getElementById('hour-hand').style.mozTransform    = 'rotate('+hour_deg+'deg)';
-        document.getElementById('hour-hand').style.msTransform     = 'rotate('+hour_deg+'deg)';
-        document.getElementById('hour-hand').style.oTransform      = 'rotate('+hour_deg+'deg)';
-        document.getElementById('hour-hand').style.transform       = 'rotate('+hour_deg+'deg)';
-    }
-    setTimeout(updateClock, 1000);
+	var hour_str = hour;
+	var minute_str = minute;
+	var second_str = second;
+	if (hour < 10) {
+		if (hour_str[0] != '0') {
+			hour_str = "0" + hour;
+		}
+	}
+	if (minute < 10) {
+		if (minute_str[0] != '0') {
+			minute_str = "0" + minute;
+		}
+	}
+	if (second < 10) {
+		if (second_str[0] != '0') {
+			second_str = "0" + second;
+		}
+	}
+	hex = '#' + hour_str + minute_str + second_str;
+	time = hour_str + ':' + minute_str + ':' + second_str;
+	$('minute-hand').style.backgroundColor = hex;
+	$('hour-hand').style.backgroundColor = hex;
+	$('hour').innerHTML = hour_str;
+	$('minute').innerHTML = minute_str;
+	$('second').innerHTML = second_str;
 }
-function invertColor(hex, bw) {
-    if (hex.indexOf('#') === 0) {
-        hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
-    }
-    var r = parseInt(hex.slice(0, 2), 16),
-        g = parseInt(hex.slice(2, 4), 16),
-        b = parseInt(hex.slice(4, 6), 16);
-    if (bw) {
-        // http://stackoverflow.com/a/3943023/112731
-        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-            ? '#000000'
-            : '#FFFFFF';
-    }
-    // invert color components
-    r = (255 - r).toString(16);
-    g = (255 - g).toString(16);
-    b = (255 - b).toString(16);
-    // pad each with zeros and return
-    return "#" + padZero(r) + padZero(g) + padZero(b);
-}
-updateClock();
